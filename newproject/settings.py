52faa476c9c98b36a035a7b02565cc67a322c9ec
Esakkinathan <105111413+Esakkinathan@users.noreply.github.com>
2023-08-22 13:25:12 +0530

    Update settings.py

--- newproject/settings.py
+++ newproject/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'newproject.wsgi.application'
 
-
+CSRF_TRUSTED_ORIGINS = ['https://eventing-kqos.zeet-esakkinathan03.zeet.app/']
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
